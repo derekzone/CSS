bb9ac88a1251dcbd57b6a452314ba0615480a9fa
Derek <derekzone@163.com>
2018-03-24 19:46:09 +0800

    add logout

--- src/main/java/com/derek/controller/LoginLogoutController.java
+++ src/main/java/com/derek/controller/LoginLogoutController.java
@@ -1,7 +1,10 @@
 package com.derek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Derek
@@ -12,6 +15,13 @@ public class LoginLogoutController {
 
     @RequestMapping("/login")
     public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/logout")
+    public String logout(ModelMap modelMap, HttpSession httpSession) {
+        modelMap.remove("user");
+        httpSession.removeAttribute("user");
         return "login";
     }
 }
